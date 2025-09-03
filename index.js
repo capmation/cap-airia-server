82cb37609faf7828c44a20bf42746ba31ae4b11c
Nicolas Vela <nicolasvelah@gmail.com>
2025-09-03 15:31:22 -0600

    allowedOrigins

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(express.json());
 
 const allowedOrigins = [
-  "http://localhost:5173"
+  "http://localhost:5173",
+  "https://cap-airia-server.onrender.com",
 ];
 
 app.use(
